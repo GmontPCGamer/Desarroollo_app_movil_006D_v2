9fdb422ec145595b0792139f7cf7ce0bb97c4785
Koveb-dev <ce.flores@duocuc.cl>
2025-12-12 10:28:45 -0300

    Se aplica un cambio al nombre de la clase de ComentarioRepository

---
+++ app/src/main/java/com/example/proyectologin006d_final/data/repository/ComentarioRepository.kt
@@ -0,0 +1,14 @@
+package com.example.proyectologin006d_final.data.repository
+
+import com.example.proyectologin006d_final.data.model.Comentario
+import com.example.proyectologin006d_final.data.remote.RetrofitInstance
+
+class ComentarioRepository {
+    suspend fun obtenerComentarios(): List<Comentario> =
+        RetrofitInstance.api.obtenerComentarios()
+
+    suspend fun obtenerComentariosPorPublicacion(postId: Int): List<Comentario> =
+        RetrofitInstance.api.obtenerComentariosPorPublicacion(postId)
+}
+
+//
